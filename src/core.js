5f9474981fe378b4d2e485a73201a7e2e64d53e4
Haley Townsend <haleyrk@hotmail.com>
2018-03-21 13:01:04 -0500

    reducer

--- src/core.js
+++ src/core.js
@@ -16,16 +16,24 @@ function getWinners(vote) {
 
 export function next(state) {
   const entries = state.get('entries').concat(getWinners(state.get('vote')));
-  return state.merge({
-    vote: Map({pair: entries.take(2)}),
-    entries: entries.skip(2)
-  });
+  if(entries.size === 1) {
+    return state.remove('vote')
+                .remove('entries')
+                .set('winner', entries.first());
+  } else {
+    return state.merge({
+      vote: Map({pair: entries.take(2)}),
+      entries: entries.skip(2)
+    });
+  }
 }
 
 export function vote(state, entry) {
   return state.updateIn(
-    ['vote', 'tally', entry],
+    ['tally', entry],
     0,
     tally => tally + 1
   );
 }
+
+export const INITIAL_STATE = Map();
