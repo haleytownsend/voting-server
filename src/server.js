4a7e2fa55efb09ae19f39b38928f502d24d10749
Haley Townsend <haleyrk@hotmail.com>
2018-03-21 14:55:42 -0500

    entries & last server updates

--- src/server.js
+++ src/server.js
@@ -1,5 +1,16 @@
 import Server from 'socket.io';
 
-export default function startServer() {
+export function startServer(store) {
   const io = new Server().attach(8090);
+  //send application state to client as changes
+  store.subscribe(
+    () => io.emit('state', store.getState().toJS())
+  );
+  //send current application state when client connects
+  //clients remote actions are fwd'd back as action events
+  io.on('connection', (socket) => {
+    socket.emit('state', store.getState().toJS());
+    socket.on('action', store.dispatch.bind(store));
+  });
+
 }
